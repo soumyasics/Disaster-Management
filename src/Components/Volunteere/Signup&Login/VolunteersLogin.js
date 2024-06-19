0da99d4746cc2a23e53b38a33fd08b60e7730094
Beninsha27 <beninsharb@gmail.com>
2024-06-19 12:03:17 +0530

    disaster

--- src/Components/Volunteere/Signup&Login/VolunteersLogin.js
+++ src/Components/Volunteere/Signup&Login/VolunteersLogin.js
@@ -1,12 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './VolunteersLogin.css'
 import fireimg from "../../../Assets/fire-image.png";
 import logo from '../../../Assets/WebGuard-Logo.png'
 import { Link } from 'react-router-dom';
 import Navbar from '../../User/Navbar/Navbar';
 import Footer from '../../User/Footer/Footer';
+import axiosInstance from '../../Constants/Baseurl';
 
 function VolunteersLogin() {
+    const [data, setData] = useState({ email: '', password: '' });
+    const [errors, setErrors] = useState({ email: '', password: '' });
+    const [formIsValid, setFormIsValid] = useState(true);
+
+
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+      setData(prevData => ({
+          ...prevData,
+          [name]: value
+      }));
+      setErrors(prevErrors => ({
+          ...prevErrors,
+          [name]: ''
+      }));
+  };
+
+  const validateField = (fieldName, value) => {
+    if (!value.trim()) {
+        setFormIsValid(false);
+        return `${fieldName} is required`;
+    }
+    return '';
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  let errors = {};
+  let formIsValid = true;
+
+  errors.email = validateField('Email', data.email);
+  if (errors.email) formIsValid = false;
+
+  errors.password = validateField('Password', data.password);
+  if (errors.password) formIsValid = false;
+
+  setErrors(errors);
+  setFormIsValid(formIsValid);
+
+  if (formIsValid) {
+      console.log("data", data);
+      axiosInstance.post('/volunteerlogin', data)
+          .then(response => {
+              console.log("Response:", response);
+              if (response.data.status === 200) {
+                  console.log("Login Successful");
+                  alert("Login Successful");
+                  // localStorage.setItem('junioradvocateId',response.data.data._id)
+              } else {
+                  console.log("Login Failed");
+                  alert("Login Failed");
+              }
+          })
+          .catch(error => {
+              console.error("There was an error!", error);
+          });
+  }
+};
   return (
     <div>
         <Navbar/>
@@ -21,18 +80,30 @@ function VolunteersLogin() {
         </div>
         <div className='col-lg-7 col-md-6 col-sm-12'>
           <div className='box-style-login-vol'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='vol-logo-section'>
                     <img src={logo}/>
                     <p>Login</p>
                 </div>
             <div className='col-12 pb-3'>
                     <p className='login-vol'>UserName:</p>
-                    <input type='text' className='form-control input-type-change-vol' placeholder='Enter Username' name='email'/>
+                    <input type='text' 
+                    className='form-control input-type-change-vol' 
+                    placeholder='Enter Username' 
+                    name='email'
+                    value={data.name}
+                    onChange={handleChange}
+                    />
                     </div>
                     <div className='col-12 pb-3'>
                     <p className='login-vol'>Password:</p>
-                    <input type='password' className='form-control input-type-change-vol' placeholder='Enter password' name='password'/>
+                    <input type='password' 
+                    className='form-control input-type-change-vol' 
+                    placeholder='Enter password' 
+                    name='password'
+                    value={data.name}
+                    onChange={handleChange}
+                    />
                     <div className='div-left-vol'>
                         <Link className='link-style-change-vol'>Forgot Password?</Link>
                     </div>
