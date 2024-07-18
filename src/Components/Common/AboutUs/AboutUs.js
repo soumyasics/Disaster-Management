8f3f7b430506db60f335b27614f307469999bddd
Abinesh RS <abiabinesh483.com>
2024-07-18 10:42:36 +0530

    aboutpage

--- src/Components/Common/AboutUs/AboutUs.js
+++ src/Components/Common/AboutUs/AboutUs.js
@@ -4,12 +4,12 @@ import about from '../../../Assets/image 29.png';
 function AboutUs() {
     return (
         <div className='col-12 about_bg'>
-            <div class="  container text-center pt-5">
-                <div class="row">
+            <div className="  container text-center pt-5">
+                <div className="row">
                    <p className='about_text_col'>AboutUs</p>
                 </div>
-                <div class="row justify-content-center mt-5">
-                  <img className='about_img img-fluid' src={about}/>
+                <div className="row justify-content-center mt-5">
+                  <img className='about_img img-fluid' src={about} alt='About image'/>
                 </div>
                 <div className='row mt-5'>
                     <p className='about_bottom_text mt-5 text-break'>As a member of our rescue team, you play a crucial role in saving lives and providing aid during disasters. Our platform is designed to support you with 
